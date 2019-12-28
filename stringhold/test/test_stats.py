f5b6b003ac97602c125c33f3db3415033fb114e8
Norbaeocystin <baranrastislav@gmail.com>
2019-12-28 18:49:42 +0100

    Add files via upload

---
+++ stringhold/test/test_stats.py
@@ -0,0 +1,27 @@
+from .. import Stats
+
+stats = Stats()
+
+def test_alpha():
+    result = stats.get_alpha_ratio('string')
+    assert result == 100.0
+    result = stats.get_alpha_ratio('Acta Non Verba.')
+    assert result == 80.0
+    result = stats.get_alpha_ratio('12345')
+    assert result == 0.0
+    
+def test_digit():
+    result = stats.get_digit_ratio('string')
+    assert result == 0.0
+    result = stats.get_digit_ratio('1. Acta Non Verba.')
+    assert result == 5.555555555555555
+    result = stats.get_digit_ratio('12345')
+    assert result == 100.0
+    
+def test_ascii():
+    result = stats.get_ascii_ratio('string')
+    assert result == 100.0
+    result = stats.get_ascii_ratio('Câ\x82\x81â\x82\x85Hâ\x82\x82â\x82\x84O')
+    assert result == 20.0
+    result = stats.get_ascii_ratio('https://www.oglaf.com/brimstone-vinegar/')
+    assert result == 100.0
